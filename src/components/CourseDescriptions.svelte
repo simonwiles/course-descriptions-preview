d830826d0e22b7e7722c5c47a8c1cf51129146c5
Simon Wiles <simonjwiles@gmail.com>
2024-08-31 10:22:31 -0700

    Initial iframe implementation

--- src/components/CourseDescriptions.svelte
+++ src/components/CourseDescriptions.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { pb } from '$lib/pocketbase';
-	let { institution, subjectAreaCode } = $props();
+	let { institution, subjectAreaCode, upstreamSource = $bindable() } = $props();
 
 	console.log(institution, subjectAreaCode);
 	const getCourseDescriptions = () => {
@@ -23,7 +23,52 @@
 			</header>
 			<p>{courseDescription.course_description}</p>
 
-			<a href={courseDescription.source} target="_blank">{courseDescription.source}</a>
+			<div>
+				<a href={courseDescription.source} target="_blank">
+					{courseDescription.source}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
+						<path d="M11 13l9 -9" />
+						<path d="M15 4h5v5" />
+					</svg></a
+				>
+				<a
+					href={courseDescription.source}
+					target="_blank"
+					onclick={(e) => {
+						e.preventDefault();
+						upstreamSource = courseDescription.source;
+					}}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path
+							d="M12 9v-3.586a1 1 0 0 1 1.707 -.707l6.586 6.586a1 1 0 0 1 0 1.414l-6.586 6.586a1 1 0 0 1 -1.707 -.707v-3.586h-3v-6h3z"
+						/>
+						<path d="M3 9v6" />
+						<path d="M6 9v6" />
+					</svg></a
+				>
+			</div>
 		</section>
 	{/each}
 {/await}
@@ -34,9 +79,25 @@
 		padding: 1rem;
 	}
 
+	div {
+		align-items: center;
+		display: flex;
+		gap: 0.5rem;
+		justify-content: flex-end;
+	}
+
 	a {
-		display: inline-block;
+		align-items: center;
+		border: 2px solid transparent;
+		border-left: none;
+		border-right: none;
+		display: flex;
+		gap: 0.5rem;
+		padding-bottom: 2px;
 		text-align: right;
-		width: 100%;
+
+		&:hover {
+			border-color: var(--primary-color);
+		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,13 @@
 
 	let currentInstitution = $state();
 	let currentSubjectAreaCode = $state();
+	let upstreamSource = $state();
 
 	const gridTemplateColumns = () => {
-		if (currentSubjectAreaCode) return '1fr 1fr 4fr';
-		if (currentInstitution) return '1fr 2fr 0fr';
-		return '1fr 0fr 0fr';
+		if (upstreamSource) return '0fr 1fr 4fr 5fr';
+		if (currentSubjectAreaCode) return '1fr 1fr 4fr 0fr';
+		if (currentInstitution) return '1fr 2fr 0fr 0fr';
+		return '1fr 0fr 0fr 0fr';
 	};
 </script>
 
@@ -29,9 +31,39 @@
 			<CourseDescriptions
 				institution={currentInstitution}
 				subjectAreaCode={currentSubjectAreaCode}
+				bind:upstreamSource
 			/>
 		{/if}
 	</div>
+
+	{#if upstreamSource}
+		<iframe
+			src={'https://course-descriptions.yaffle.xyz/proxy?url=' + encodeURIComponent(upstreamSource)}
+			title="Course Descriptions"
+		></iframe>
+		<div class="institution-title">
+			<button onclick={() => (upstreamSource = null)}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path
+						d="M12 15v3.586a1 1 0 0 1 -1.707 .707l-6.586 -6.586a1 1 0 0 1 0 -1.414l6.586 -6.586a1 1 0 0 1 1.707 .707v3.586h3v6h-3z"
+					/>
+					<path d="M21 15v-6" />
+					<path d="M18 15v-6" />
+				</svg>
+				{currentInstitution?.institution_name}
+			</button>
+		</div>
+	{/if}
 </div>
 
 <style>
@@ -60,6 +92,7 @@
 			background-color: transparent;
 			border-radius: 0;
 			border: none;
+			cursor: pointer;
 			font: inherit;
 			margin: 0;
 			padding: 1rem;
@@ -93,6 +126,10 @@
 		transition: 500ms;
 		width: calc(100vw - 8rem);
 
+		grid-template-areas:
+			'institutions institution-title institution-title iframe'
+			'institutions subject-area-codes course-descriptions iframe';
+
 		& > div {
 			background-color: white;
 			display: flex;
@@ -101,5 +138,57 @@
 			overflow-x: hidden;
 			overflow-y: auto;
 		}
+
+		.institution-title {
+			box-sizing: border-box;
+			flex-direction: row;
+			width: 100%;
+			grid-area: institution-title;
+			font-size: 1.4rem;
+			background-color: var(--primary-color);
+			color: #fff;
+			font-weight: bold;
+			padding: 0.5rem 1rem;
+
+			button {
+				align-items: center;
+				appearance: none;
+				background-color: transparent;
+				border-radius: 0;
+				border: none;
+				color: inherit;
+				cursor: pointer;
+				display: flex;
+				font: inherit;
+				gap: 1rem;
+
+				svg {
+					transition: transform 100ms;
+				}
+
+				&:hover svg,
+				&:focus svg {
+					transform: translateX(-4px);
+				}
+			}
+		}
+	}
+
+	.institutions {
+		grid-area: institutions;
+	}
+
+	.subject-area-codes {
+		grid-area: subject-area-codes;
+	}
+
+	.course-descriptions {
+		grid-area: course-descriptions;
+	}
+
+	iframe {
+		height: 100%;
+		width: 100%;
+		grid-area: iframe;
 	}
 </style>
